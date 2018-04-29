5ea70e59acc56bcdd882ed65b71083d699e24bcd
evanengelhardt <eaengelh@gmail.com>
2018-04-29 11:27:45 -0500

    Started on the frontend development using angular CLI to create all the components, created routes in app.module, and added styling from bootswatch and html for the navbar from bootstrap.

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'register', component: RegisterComponent}
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
