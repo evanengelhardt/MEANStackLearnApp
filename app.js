1f8eef8957c2a6df8e8785069c7d246a19d01e9e
evanengelhardt <eaengelh@gmail.com>
2018-04-29 21:49:48 -0500

    Finished app, built into public folder. Added ability to get profiles as well as protecting routes with guarding

--- app.js
+++ app.js
@@ -49,6 +49,10 @@ app.get('/', (req, res) => {
   res.send('Invalid Endpoint');
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
 app.listen(port, () => {
   console.log('server started on port: ' + port);
 });
--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function(passport){
   opts.secretOrKey = config.secret;
 
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    User.getUserById(jwt_payload.data._id, (err, user) => {
+    User.getUserByID(jwt_payload.data._id, (err, user) => {
       if(err){
         return done(err, false);
       }
